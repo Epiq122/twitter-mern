7f5ffeaeebbaccace6b1778973923d8976f087a4
Robert Gleason <l2ob122@gmail.com>
2024-04-25 19:37:07 -0700

    Add authentication routes and controller functions

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signUp = async (req, res) => {
+  res.json({ message: 'Signup route' });
+};
+
+export const login = async (req, res) => {
+  res.json({ message: 'logout route' });
+};
+
+export const logout = async (req, res) => {
+  res.json({ message: 'logout route' });
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { login, logout, signUp } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-  res.json({ message: 'Signup route' });
-});
+router.post('/signup', signUp);
+
+router.post('/login', login);
+
+router.post('/logout', logout);
 
 export default router;
