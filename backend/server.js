77c1aa0f22072759c2f1df0bae6aa1b1f61f7335
Robert Gleason <l2ob122@gmail.com>
2024-04-25 19:52:49 -0700

    Add MongoDB connection and update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import authRoutes from './routes/auth.routes.js';
+import dotenv from 'dotenv';
 
+import authRoutes from './routes/auth.routes.js';
+import connectMongoDB from './db/connectMongoDB.js';
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use('/api/auth', authRoutes);
 
-app.listen(8000, () => {
-  console.log('Server started at http://localhost:8000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectMongoDB();
 });
